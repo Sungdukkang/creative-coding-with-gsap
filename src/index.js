34ee8699078b975425dec58ff355a3a821ef0eae
sungduk kang <sungduk.kang.94@gmail.com>
2025-01-30 04:56:12 -0800

    grid component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import { Clock } from './clock';
 import { Marquee } from './marquee';
+import { Grid } from './grid';
 
 import { reveal } from './helpers/reveal';
 
 const clock = new Clock('.hero_clock');
+const grid = new Grid();
 
 // Wait for fonts to load
 document.fonts.ready.then(() => {
+  // marquees
   const marqueeElements = document.querySelectorAll('.marquee');
   marqueeElements.forEach((marqueeElement, i) => {
     const marquee = new Marquee(marqueeElement, {
       pauseOnHover: true,
       reversed: i % 2 === 0,
       scrollTrigger: true,
-    });    
+    });
   });
-
 });
 
 reveal();
