046efed6ba1dbea582c5d8691ebcce97e0032ba7
sungduk kang <sungduk.kang.94@gmail.com>
2025-02-08 00:56:52 -0800

    toolkit animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Clock } from './clock';
 import { Marquee } from './marquee';
 import { Grid } from './grid';
 import { Heading } from './heading';
+import { ToolkitText } from './toolkit';
 
 import { reveal } from './helpers/reveal';
 
@@ -10,9 +11,6 @@ const grid = new Grid();
 
 // Wait for fonts to load
 document.fonts.ready.then(() => {
-  // animated heading
-  const heading = new Heading(document.querySelector(".hero_h1"));
-
   // marquees
   const marqueeElements = document.querySelectorAll('.marquee');
   marqueeElements.forEach((marqueeElement, i) => {
@@ -22,6 +20,11 @@ document.fonts.ready.then(() => {
       scrollTrigger: true,
     });
   });
-});
 
-reveal();
+  // other elements
+  const heading = new Heading(document.querySelector(".hero_h1"));
+  const toolkit = new ToolkitText(".toolkit_p");
+
+  // prevent FOUC
+  reveal();
+});
